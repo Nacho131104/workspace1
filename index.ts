18adc8621202cf46114411b6a2440db0c9163ced
Nacho <nacholopezamat@gmail.com>
2025-10-17 13:29:06 -0400

    ejemplos funciones conawait

--- index.ts
+++ index.ts
@@ -32,4 +32,50 @@ const getPersonaje = async (id:number) =>{
 }
 
 
-const morty = await getPersonaje(1)
+//const morty = await getPersonaje(1)
+
+
+// si en algun momento alguno se rompe, el programa no sigue
+const getCharacters = async(ids:number[])=>{
+    ids.forEach(async(id) =>{
+        const personaje = await getPersonaje(id)
+        console.log(personaje.data)
+    })
+}
+
+//onst personajes = await getCharacters([1,2,3])
+
+
+const getMultipleCharacters = async(ids:number[])=>{
+
+    const promesas = ids.map(async(id)=>
+        (await axios.get("https://rickandmortyapi.com/api/character/"+id)).data
+    )
+    const responses = await Promise.all(promesas)
+    return responses
+}
+
+/*
+const personajes = getMultipleCharacters([1,2,3])
+console.log(personajes)
+*/
+
+
+//la diferencia es que ademas te muestra si ha llegado con el status
+const getSafeCharacters = async (ids:number[]) =>{
+    const promesas = ids.map(async(id)=>
+        (await axios.get("https://rickandmortyapi.com/api/character/"+id)).data
+    )
+    const responses = await Promise.allSettled(promesas)
+
+    responses.forEach((response)=>{
+        if(response.status =="fulfilled"){
+            console.log(response.value)
+        }else{
+            console.log("Error")
+        }
+    })
+}
+
+
+getSafeCharacters([1,2,3])
