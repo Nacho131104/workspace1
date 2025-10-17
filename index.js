cfc72bf4c50c29e3c8e126f5bee95a5877d6948b
Nacho <nacholopezamat@gmail.com>
2025-10-17 12:45:54 -0400

    example await

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const personaje1 = fetch("https://rickandmortyapi.com/api/character/2");
+personaje1.then((response) => {
+    const data = response.json();
+    data.then((personaje) => {
+        console.log(personaje);
+    });
+}).catch((error) => {
+    console.log("Error en la peticion");
+});
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosError } from "axios";
 
+
+/* Esto es sin axios
 const personaje1 =  fetch("https://rickandmortyapi.com/api/character/2")
 
 
@@ -12,3 +15,21 @@ personaje1.then((response)=>{
 
     console.log("Error en la peticion")
 })
+*/
+
+// forma correcta de hacer una llamada con axios y mostrar el resultado
+const getPersonaje = async (id:number) =>{
+    try{
+        const personaje1 = await axios.get("https://rickandmortyapi.com/api/character/"+id)
+        return personaje1.data 
+    }catch(err){
+        if(axios.isAxiosError(err)){
+            console.log("Error de axios: "+err)
+        }else{
+            console.log("Error: "+err)
+        }
+    }
+}
+
+
+const morty = await getPersonaje(1)
